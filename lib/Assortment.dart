c09655a910dfc75a17e6ebc356c56125a9163852
Christopher Best <bestchris@gmail.com>
2013-10-29 13:01:12 -0400

    moved source file to lib dir

--- lib/Assortment.dart
+++ lib/Assortment.dart
@@ -1,7 +1,7 @@
 part of Tabasci;
 
 // TODO can we subclass from Event?
-/// An event that occurs during a drag on a sortable element 
+/// An event that occurs during a drag on a sortable element
 // TODO event type code?
 class AssortmentEvent {
   /// The [MouseEvent] that instigated this event
@@ -12,7 +12,7 @@ class AssortmentEvent {
   Element enterElement;
   /// The [Element] the cursor is coming from
   Element fromElement;
-  
+
   AssortmentEvent(MouseEvent this.mouseEvent, Element this.dragElement,
       { Element this.enterElement, Element this.fromElement }) {}
 }
@@ -24,18 +24,18 @@ class Assortment {
   Assortment(Element parent) {
     _parent = parent;
   }
-  
+
   Element _parent;
   /// The parent element of the sortable elements
   Element get parent => _parent;
-  
+
   // The set of elements that are sortable
   Set<Element> _elements = new Set();
   // The last element that the cursor entered
   Element _fromElement;
   // The element begin dragged
   Element _dragElement;
-  
+
   // Expose event streams
   StreamController<AssortmentEvent> _dragStartStreamController = new StreamController<AssortmentEvent>();
   Stream<AssortmentEvent> get onDragStart => _dragStartStreamController.stream;
@@ -43,15 +43,15 @@ class Assortment {
   Stream<AssortmentEvent> get onDragEnter => _dragEnterStreamController.stream;
   StreamController<AssortmentEvent> _dragEndStreamController = new StreamController<AssortmentEvent>();
   Stream<AssortmentEvent> get onDragEnd => _dragEndStreamController.stream;
-  
+
   /// Add an element to the assortment
   void addElement(Element element) {
     // add to the element set
     _elements.add(element);
-    
+
     // make line draggable
     element.attributes["draggable"] = "true";
-    
+
     // add drag start handler
     element.onDragStart.listen((event) {
       // use the drag icon for moving
@@ -67,20 +67,20 @@ class Assortment {
     element.onDragOver.listen((event) {
       event.preventDefault();
     });
-    
+
     // add enter handler
     // TODO check that dragged element is in the assortment
     element.onDragEnter.listen((MouseEvent event) {
-      
+
       // cache the current from element
       Element fromElement = _fromElement;
-      
+
       // update the from element
       _fromElement = event.target;
-      
+
       // if we are moving within a single sortable element, bail
       if((event.currentTarget as Element).contains(fromElement)) return;
-      
+
       // get the index of the element begin dragged
       int dragIndex = parent.children.indexOf(_dragElement);
       // get the index of the element being dragged into
@@ -91,11 +91,11 @@ class Assortment {
       } else {
         (event.currentTarget as Element).insertAdjacentElement("beforeBegin", _dragElement);
       }
-      
+
       // add assortment event to stream
       _dragEnterStreamController.add(new AssortmentEvent(event, _dragElement, enterElement: event.currentTarget, fromElement: _fromElement));
     });
-    
+
     // add end handler
     element.onDragEnd.listen((event) {
       // add assortment event to stream
